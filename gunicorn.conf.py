be71829fd1f4e2af8c8c3f30d053f5777acfe7d7
Max <32428314+benkenobi3@users.noreply.github.com>
2020-03-26 13:29:27 +0300

    gunicorn port fix

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,6 +4,6 @@ import multiprocessing
 DJANGO_ENV = os.environ.get('DJANGO_ENV', 'DEBUG')
 DEBUG = DJANGO_ENV == 'DEBUG'
 
-bind = '0.0.0.0:' + os.environ.get('DJANGO_APP_PORT', '8000')
+bind = '0.0.0.0:' + os.environ.get('DJANGO_APP_PORT', '5000')
 log_level = 'debug' if DEBUG else 'info'
 proc_name = 'gunicorn-music'

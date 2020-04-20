e4c539aea68ed5424c0e3e573b41643c2046940e
Max <32428314+benkenobi3@users.noreply.github.com>
2020-04-20 18:25:54 +0300

    AllowAny

--- app/settings.py
+++ app/settings.py
@@ -86,7 +86,7 @@ TEMPLATES = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
